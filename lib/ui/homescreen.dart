59bd29aff82d6ed83632cda68bd44af04529247d
Fahad Mehboob <fahad.fakky@hotmail.com>
2024-03-04 18:58:06 +0500

    home screen UI changes display name showing on homescreen

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -1,6 +1,9 @@
 import 'package:aviation_project/services/google_auth_service.dart';
 import 'package:aviation_project/ui/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/reusable_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,7 +12,11 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
 class _HomeScreenState extends State<HomeScreen> {
+  final userName = _auth.currentUser?.displayName ?? "Guest";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,19 +24,23 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Home Screen"),
         actions: [
           InkWell(
-              onTap: () {
-                GoogleAuthService().signOut(context);
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginScreen(),
-                    ));
-              },
-              child: const Icon(Icons.logout_sharp))
+            onTap: () {
+              GoogleAuthService().signOut(context);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
+                  ));
+            },
+            child: const Icon(Icons.logout_sharp),
+          ),
         ],
       ),
       body: Column(
-        children: const [],
+        children: [
+          ReusableTitle(title: "Hello $userName"),
+          const ReusableSubTitle(subTitle: "Welcome back! Let's get started."),
+        ],
       ),
     );
   }
